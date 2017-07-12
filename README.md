921f84752bbeda13941a24e778c2858ad97e9979
Levi Holmes <levi.holmes81@gmail.com>
2017-07-12 16:49:42 -0600

    end of day push

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.pivotaltracker.com/n/projects/2077565
+
+https://dashboard.heroku.com/apps/arcane-sands-57194
--- js/init.js
+++ js/init.js
@@ -1,3 +1,8 @@
+$("document").ready(function() {
+  $("button").click(function() {
+  })
+});
+
 (function($){
   $(function(){
 
@@ -5,3 +10,28 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var MenupullAPI = "https://galvanize-eats-api.herokuapp.com/menu"
+var $SetSelect = $('#gEatsMenu')
+
+$.get(MenupullAPI)
+  .then(function(data){
+    console.log(data);
+for (var i = 0; i < data.menu.length; i++) {
+  $SetSelect.append("<option><p>" + data.menu[i].name + "</p></option>")
+}$(".menuContainer").empty();
+for (var i = 0; i < data.menu.length; i++) {
+       var menui = data.menu[i];
+       var menupulldown = menui.name + menui.price;
+       $(menuContainer).append(name + price)}
+})
+
+
+
+$.get(gEatsMenu.then(function(data) {
+    var cards = data.menu[i].price;
+    for (var i = 0; i < menu.length; i++) {
+      updatePage(menu[i]);
+        }
+      })
+    )
